937f655d6cc99ef53593d1b79ae68c7cecd6655f
felipefk29 <felipefk@al.insper.edu.br>
2026-02-23 21:44:34 -0300

    Fix initialization of variable 'a' in main.c to prevent uninitialized variable warnings

--- exe0_cppcheck/main.c
+++ exe0_cppcheck/main.c
@@ -9,12 +9,12 @@ int multiplyNumbers(int x, int y) {
 }
 
 int main(void) {
-    int a = 5;
+    int a = 0;
     int b = 5;
      //uninitvar, legacyUninitvar
     int result = multiplyNumbers(a, b);
     (void)result; // Suppress unused variable warning
-    
+
     int arr[NUM_ELEMENTS];
     for(int i = 0; i < NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
         arr[i] = i;
