77c0f2a4cdbbde7087c09ef158c9abe9e5726b9e
felipefk29 <felipefk@al.insper.edu.br>
2026-02-23 21:42:04 -0300

    Refactor LED control in main.c to initialize and toggle both red and yellow LEDs

--- exe0_cppcheck/main.c
+++ exe0_cppcheck/main.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NUM_ELEMENTS 5
+#define NUM_ELEMENTS 10
 
 int multiplyNumbers(int x, int y) {
     int product = x * y;
+    return product;
 }
 
 int main(void) {
-    int a, b = 5; //uninitvar, legacyUninitvar
+    int a = 5;
+    int b = 5;
+     //uninitvar, legacyUninitvar
     int result = multiplyNumbers(a, b);
-
+    (void)result; // Suppress unused variable warning
+    
     int arr[NUM_ELEMENTS];
-    for(int i = 0; i <= NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
+    for(int i = 0; i < NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
         arr[i] = i;
     }
 
--- exe0_variables/main.c
+++ exe0_variables/main.c
@@ -1,16 +1,16 @@
 #include "stdio.h"
 
-int a = 0; // var global
-int b = 0; // var global
-
-void foo(void) {
-    a = a + 1; // acessa variavel global
-}
-
 void main(void) {
+    int a = 0;
+    int b = 0;
+
+    void foo(void) {
+        a = a + 1; // acessa variavel global
+    }
+
     while (1) {
         foo();
-        
+
         if (a > 5) {
             b = 1;
         }
