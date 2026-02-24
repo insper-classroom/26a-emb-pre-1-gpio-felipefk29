b6184fb5f045b8d6625fdbc1b9fab5037dfbd4c0
felipefk29 <felipefk@al.insper.edu.br>
2026-02-23 22:11:31 -0300

    ex 3

--- exe3/main.c
+++ exe3/main.c
@@ -2,9 +2,46 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int LED_PIN_R = 4;
+const int LED_PIN_G = 6;
+const int BTN_PIN_RED = 28;
+const int BTN_PIN_GREEN = 26;
+
 int main() {
   stdio_init_all();
 
+  gpio_init(LED_PIN_R);
+  gpio_init(LED_PIN_G);
+  gpio_init(BTN_PIN_RED);
+  gpio_init(BTN_PIN_GREEN);
+
+  gpio_set_dir(LED_PIN_R, GPIO_OUT);
+  gpio_set_dir(LED_PIN_G, GPIO_OUT);
+  gpio_set_dir(BTN_PIN_RED, GPIO_IN);
+  gpio_set_dir(BTN_PIN_GREEN, GPIO_IN);
+
+  gpio_pull_up(BTN_PIN_RED);
+  gpio_pull_up(BTN_PIN_GREEN);
+
+  gpio_put(LED_PIN_R, 0);
+  gpio_put(LED_PIN_G, 0);
+
+  int led_state_r = 0;
+  int led_state_g = 0;
+
   while (true) {
+    if (!gpio_get(BTN_PIN_RED)) {
+      led_state_r = !led_state_r;
+      gpio_put(LED_PIN_R, led_state_r);
+      printf("Botao Vermelho\n");
+      while (!gpio_get(BTN_PIN_RED)) {
+      };
+    } else if (!gpio_get(BTN_PIN_GREEN)) {
+      led_state_g = !led_state_g;
+      gpio_put(LED_PIN_G, led_state_g);
+      printf("Botao Verde\n");
+      while (!gpio_get(BTN_PIN_GREEN)) {
+      };
+    }
   }
 }
