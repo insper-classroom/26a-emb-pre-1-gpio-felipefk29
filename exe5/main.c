043eb9288c9e1a4939807e1e3080b6914428b3c9
felipefk29 <felipefk@al.insper.edu.br>
2026-02-23 23:06:21 -0300

    Ex 6

--- exe5/main.c
+++ exe5/main.c
@@ -1,48 +1,51 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
-#include <stdbool.h>
 
 const int BTN_PIN = 26;
 const int BTN_PIN_2 = 7;
 
-static bool debounce(int pin){
-    if (gpio_get(pin)) {
-        return false;
-    }
-    sleep_ms(50);
-    if (gpio_get(pin)) {
-        return false;
-    }
-    
-    while (!gpio_get(pin)) {
-    }
-    sleep_ms(50);
+int main(void) {
+  stdio_init_all();
 
-    return true;
-}
+  gpio_init(BTN_PIN);
+  gpio_set_dir(BTN_PIN, GPIO_IN);
+  gpio_pull_up(BTN_PIN);
 
+  gpio_init(BTN_PIN_2);
+  gpio_set_dir(BTN_PIN_2, GPIO_IN);
+  gpio_pull_up(BTN_PIN_2);
 
-int main() {
-    stdio_init_all();
+  int cnt_1 = 0;
+  int cnt_2 = 0;
 
-    gpio_init(BTN_PIN);
-    gpio_set_dir(BTN_PIN, GPIO_IN);
-    gpio_pull_up(BTN_PIN);
+  const int DEBOUNCE_MS = 30;
 
-    gpio_init(BTN_PIN_2);
-    gpio_set_dir(BTN_PIN_2, GPIO_IN);
-    gpio_pull_up(BTN_PIN_2);
-    int cnt_1 = 0;
-    int cnt_2 = 0;
-
-    while (true) {
-
-        if (debounce(BTN_PIN)) {
-            printf("Botao 1: %d\n", cnt_1++);
+  while (true) {
+    if (!gpio_get(BTN_PIN)) {
+      sleep_ms(DEBOUNCE_MS);
+      if (!gpio_get(BTN_PIN)) {
+        printf("Botao 1: %d\n", cnt_1);
+        cnt_1++;
+        while (!gpio_get(BTN_PIN)) {
+          sleep_ms(1);
         }
-        if (debounce(BTN_PIN_2)) {
-            printf("Botao 2: %d\n", cnt_2++);
-        }
+        sleep_ms(DEBOUNCE_MS);
+      }
     }
+
+    if (!gpio_get(BTN_PIN_2)) {
+      sleep_ms(DEBOUNCE_MS);
+      if (!gpio_get(BTN_PIN_2)) {
+        printf("Botao 2: %d\n", cnt_2);
+        cnt_2++;
+        while (!gpio_get(BTN_PIN_2)) {
+          sleep_ms(1);
+        }
+        sleep_ms(DEBOUNCE_MS);
+      }
+    }
+
+    sleep_ms(1);
+  }
 }
--- exe6/main.c
+++ exe6/main.c
@@ -1,51 +1,70 @@
 #include <stdio.h>
-#include "pico/stdlib.h"
-#include "hardware/gpio.h"
 
-const int BTN_PIN = 26;
-const int BTN_PIN_2 = 7;
+#include "hardware/gpio.h"
+#include "pico/stdlib.h"
+
+static const int FIRST_GPIO = 2;
+static const int BTN_PIN_G = 28;
+
+static int cnt = 0;
+static int last_btn = 1; 
+
+
+static const int bits[10] = {
+    0x3f,  // 0
+    0x06,  // 1
+    0x5b,  // 2
+    0x4f,  // 3
+    0x66,  // 4
+    0x6d,  // 5
+    0x7d,  // 6
+    0x07,  // 7
+    0x7f,  // 8
+    0x67   // 9
+};
+
+static void seven_seg_init(void) {
+    for (int gpio = FIRST_GPIO; gpio < FIRST_GPIO + 7; gpio++) {
+        gpio_init(gpio);
+        gpio_set_dir(gpio, GPIO_OUT);
+    }
+}
+
+static void seven_seg_display(void) {
+    int value = bits[cnt];
+
+    for (int i = 0; i < 7; i++) {
+        int gpio = FIRST_GPIO + i;
+        int bit = (value >> i) & 1;
+        gpio_put(gpio, bit);
+    }
+}
 
 int main(void) {
-  stdio_init_all();
+    stdio_init_all();
 
-  gpio_init(BTN_PIN);
-  gpio_set_dir(BTN_PIN, GPIO_IN);
-  gpio_pull_up(BTN_PIN);
+    gpio_init(BTN_PIN_G);
+    gpio_set_dir(BTN_PIN_G, GPIO_IN);
+    gpio_pull_up(BTN_PIN_G);
 
-  gpio_init(BTN_PIN_2);
-  gpio_set_dir(BTN_PIN_2, GPIO_IN);
-  gpio_pull_up(BTN_PIN_2);
+    seven_seg_init();
 
-  int cnt_1 = 0;
-  int cnt_2 = 0;
+    cnt = 2;              
+    seven_seg_display();
 
-  const int DEBOUNCE_MS = 30;
+    while (true) {
+        int btn = gpio_get(BTN_PIN_G);
 
-  while (true) {
-    if (!gpio_get(BTN_PIN)) {
-      sleep_ms(DEBOUNCE_MS);
-      if (!gpio_get(BTN_PIN)) {
-        printf("Botao 1: %d\n", cnt_1);
-        cnt_1++;
-        while (!gpio_get(BTN_PIN)) {
-          sleep_ms(1);
+        if (last_btn && !btn) { 
+            cnt++;
+            if (cnt > 9) {
+                cnt = 0;
+            }
+            seven_seg_display();
+            printf("cnt: %d\n", cnt);
         }
-        sleep_ms(DEBOUNCE_MS);
-      }
-    }
 
-    if (!gpio_get(BTN_PIN_2)) {
-      sleep_ms(DEBOUNCE_MS);
-      if (!gpio_get(BTN_PIN_2)) {
-        printf("Botao 2: %d\n", cnt_2);
-        cnt_2++;
-        while (!gpio_get(BTN_PIN_2)) {
-          sleep_ms(1);
-        }
-        sleep_ms(DEBOUNCE_MS);
-      }
+        last_btn = btn;
+        sleep_ms(10);
     }
-
-    sleep_ms(1);
-  }
 }
