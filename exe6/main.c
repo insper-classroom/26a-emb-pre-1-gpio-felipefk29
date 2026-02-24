856335fa4b3006b7c75df05895e72870b6a62648
felipefk29 <felipefk@al.insper.edu.br>
2026-02-23 23:17:22 -0300

    Ex 6 fixing2

--- exe6/main.c
+++ exe6/main.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-#include <stdint.h>
 #include "hardware/gpio.h"
 #include "pico/stdlib.h"
 
 static const int FIRST_GPIO = 2;
-static const int BTN_PIN_G = 28;
+static const int BTN_PIN_G  = 28;
 
-static int cnt = 0;
-static int last_btn = 1;
-
-static const uint8_t bits[10] = {
+static const int bits[10] = {
     0x3f,  // 0
     0x06,  // 1
     0x5b,  // 2
@@ -23,18 +19,23 @@ static const uint8_t bits[10] = {
 };
 
 static void seven_seg_init(void) {
-    for (int gpio = FIRST_GPIO; gpio < FIRST_GPIO + 7; gpio++) {
+    for (int gpio = FIRST_GPIO; gpio < (FIRST_GPIO + 7); gpio++) {
         gpio_init(gpio);
         gpio_set_dir(gpio, GPIO_OUT);
+        gpio_put(gpio, 0);
     }
 }
 
-static void seven_seg_display(void) {
-    uint8_t value = bits[cnt];
+static void seven_seg_display(int digit) {
+    if (digit < 0 || digit > 9) {
+        digit = 0; 
+    }
+
+    const int value = bits[digit];
 
     for (int i = 0; i < 7; i++) {
-        int gpio = FIRST_GPIO + i;
-        int bit = (value >> i) & 1;
+        const int gpio = FIRST_GPIO + i;
+        const int bit  = (value >> i) & 1;
         gpio_put(gpio, bit);
     }
 }
@@ -42,24 +43,27 @@ static void seven_seg_display(void) {
 int main(void) {
     stdio_init_all();
 
+    int cnt = 0;
+
     gpio_init(BTN_PIN_G);
     gpio_set_dir(BTN_PIN_G, GPIO_IN);
     gpio_pull_up(BTN_PIN_G);
 
     seven_seg_init();
-    seven_seg_display();
+    seven_seg_display(cnt);
 
-    last_btn = gpio_get(BTN_PIN_G); // estado inicial real
+    int last_btn = gpio_get(BTN_PIN_G); 
 
     while (true) {
-        int btn = gpio_get(BTN_PIN_G);
+        const int btn = gpio_get(BTN_PIN_G);
 
         if (last_btn && !btn) {
             cnt++;
             if (cnt > 9) {
                 cnt = 0;
             }
-            seven_seg_display();
+
+            seven_seg_display(cnt);
             printf("cnt: %d\n", cnt);
         }
 
