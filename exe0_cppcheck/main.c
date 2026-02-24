ac06362b4bc00fc65b453d3d374982c5685e6bb8
felipefk29 <felipefk@al.insper.edu.br>
2026-02-23 23:46:48 -0300

    EX 0

--- exe0_cppcheck/main.c
+++ exe0_cppcheck/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define NUM_ELEMENTS 10
+#define NUM_ELEMENTS 5
 
 static int multiplyNumbers(int x, int y) {
     return x * y;
@@ -8,15 +9,19 @@ static int multiplyNumbers(int x, int y) {
 
 int main(void) {
     int a = 0;
-    int b = 5;
-
+    int b = 5; //uninitvar, legacyUninitvar
     int result = multiplyNumbers(a, b);
-    printf("%d\n", result);   // usa a variável (evita warning)
 
     int arr[NUM_ELEMENTS];
-    for (int i = 0; i < NUM_ELEMENTS; i++) {
-        arr[i] = i;           // i vai de 0 até NUM_ELEMENTS-1 (sem estourar)
+    for(int i = 0; i < NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
+        arr[i] = i;
     }
 
+    int sum = result;
+    for (int i = 0; i < NUM_ELEMENTS; i++) {
+        sum += arr[i];
+    }
+    printf("%d\n", sum);
+    
     return 0;
 }
